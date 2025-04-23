579c070f99114c2b06d6af1fd943efaedd2a16ac
shashwat sharma <shashwatsharmapalak@gmail.com>
2025-04-23 12:43:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Welcome to my tiffin service</h1>
+      <img src='https://feeds.abplive.com/onecms/images/uploaded-images/2023/01/13/003be1298aad4ea51ff5fe981b7279d21673627124471603_original.png?impolicy=abp_cdn&imwidth=640'></img>
+    </>
+  )
+}
+
+export default App
